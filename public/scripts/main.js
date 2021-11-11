859374026eec71882bacc4b4d58b864fa84c9cbc
Caio Quiza <cai0.3duardo@gmail.com>
2021-11-11 12:03:21 -0300

    Add files via upload

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,33 @@
+//Código aprensentação slides
 document.addEventListener('DOMContentLoaded', function() {
-    var stream = document.querySelector('.temas');
-    var items = document.querySelectorAll('.apresentacao');
-    var prev = document.querySelector('.bot_ante');
-    var next = document.querySelector('.bot_prox');
+    var dev_themes = document.querySelector('.temas');
+    var scrum_text = document.querySelector('.textos');
+    var itens_software_test = document.querySelectorAll('.apresentacao');
+    var itens_method_scrum = document.querySelectorAll('.metodologia');
+    var prev_software_theme = document.querySelector('.bot_ante');
+    var prev_scrum_method = document.querySelector('.bot_scrum_ante');
+    var next_software_theme = document.querySelector('.bot_prox');
+    var next_scrum_method = document.querySelector('.bot_scrum_prox');
   
-    prev.addEventListener('click', function() {
-      stream.insertBefore(items[items.length - 1], items[0]);
-      items = document.querySelectorAll('.apresentacao');
+    prev_software_theme.addEventListener('click', function() {
+      dev_themes.insertBefore(itens_software_test[itens_software_test.length - 1], itens_software_test[0]);
+      itens_software_test = document.querySelectorAll('.apresentacao');
     });
+
+    prev_scrum_method.addEventListener('click', function() {
+        scrum_text.insertBefore(itens_method_scrum[itens_method_scrum.length - 1], itens_method_scrum[0]);
+        itens_method_scrum = document.querySelectorAll('.metodologia');
+      });
   
-    next.addEventListener('click', function() {
-      stream.appendChild(items[0]);
-      items = document.querySelectorAll('.apresentacao');
+    next_software_theme.addEventListener('click', function() {
+      dev_themes.appendChild(itens_software_test[0]);
+      itens_software_test = document.querySelectorAll('.apresentacao');
     });
+
+    next_scrum_method.addEventListener('click', function() {
+      scrum_text.appendChild(itens_method_scrum[0]);
+      itens_method_scrum = document.querySelectorAll('.metodologia');
+    });
+
+
   });
